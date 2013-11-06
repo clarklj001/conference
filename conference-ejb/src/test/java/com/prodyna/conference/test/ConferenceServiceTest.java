417f3bd60db4fa8c46d675798fb12b162dca84f5
Michael <michael.kirchmann@prodyna.de>
2013-11-06 21:05:58 +0100

    Conferencetest done

--- conference-ejb/src/test/java/com/prodyna/conference/test/ConferenceServiceTest.java
+++ conference-ejb/src/test/java/com/prodyna/conference/test/ConferenceServiceTest.java
@@ -1,6 +1,13 @@
 package com.prodyna.conference.test;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.ejb.EJBException;
 import javax.inject.Inject;
+
+import junit.framework.Assert;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -8,6 +15,7 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.prodyna.conference.model.Conference;
 import com.prodyna.conference.service.ConferenceService;
 
 @RunWith(Arquillian.class)
@@ -18,10 +26,50 @@ public class ConferenceServiceTest {
 	}
 
 	@Inject
-	private ConferenceService conferenceServiceBean;
+	private ConferenceService conferenceService;
 
 	@Test
 	public void testCrud() {
 
+		Conference conf = new Conference();
+		Date anfangsDatum = new Date();
+		conf.setAnfangsDatum(anfangsDatum);
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.add(Calendar.DATE, 7);
+		conf.setEndDatum(calendar.getTime());
+		String name = "Name";
+		String kurzBeschreibung = "Kurzbeschreibung";
+		conf.setName(name);
+		conf.setKurzBeschreibung(kurzBeschreibung);
+		conf = conferenceService.update(conf);
+
+		Assert.assertNotNull(conf.getId());
+
+		conf = conferenceService.read(conf.getId());
+		Assert.assertEquals(name, conf.getName());
+		Assert.assertEquals(kurzBeschreibung, conf.getKurzBeschreibung());
+		Assert.assertEquals(anfangsDatum, conf.getAnfangsDatum());
+		Assert.assertEquals(calendar.getTime(), conf.getEndDatum());
+
+		conferenceService.delete(conf);
+
+		Conference result = conferenceService.read(conf.getId());
+		Assert.assertNull(result);
+	}
+
+	@Test(expected = EJBException.class)
+	public void testWrongDuration() {
+		Conference conf = new Conference();
+		Date anfangsDatum = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.add(Calendar.DATE, 7);
+		conf.setEndDatum(anfangsDatum);
+		conf.setAnfangsDatum(calendar.getTime());
+		// wrong dates.
+		String name = "Name";
+		String kurzBeschreibung = "Kurzbeschreibung";
+		conf.setName(name);
+		conf.setKurzBeschreibung(kurzBeschreibung);
+		conf = conferenceService.update(conf);
 	}
 }
