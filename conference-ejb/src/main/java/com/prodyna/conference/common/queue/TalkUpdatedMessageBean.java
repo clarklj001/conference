e26dc5e04051cb7685632942ff9eecf7aba52558
Michael <michael.kirchmann@prodyna.de>
2013-11-13 21:43:36 +0100

    MDBean update

--- conference-ejb/src/main/java/com/prodyna/conference/common/queue/TalkUpdatedMessageBean.java
+++ conference-ejb/src/main/java/com/prodyna/conference/common/queue/TalkUpdatedMessageBean.java
@@ -1,7 +1,9 @@
 package com.prodyna.conference.common.queue;
 
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
 import javax.inject.Inject;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -10,13 +12,17 @@ import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 
-@MessageDriven(activationConfig = {
+@MessageDriven(mappedName = "queue/talkupdated", activationConfig = {
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/talkupdated") }, mappedName = "queue/talkupdated")
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/talkupdated") })
 public class TalkUpdatedMessageBean implements MessageListener {
 
 	@Inject
 	private Logger log;
+
+	@Resource
+	MessageDrivenContext mdc;
 
 	public void onMessage(Message message) {
 		if (message instanceof TextMessage) {
